18224ea4da45279e13825402db5e90b23c5699e6
Nha Tran <tran.nha@mulodo.com>
2015-02-13 15:16:16 +0700

    Update miniblog EER and SQL Install

---
+++ doc/backend/db/TRN-MiniBlog_DatabaseInstall_NhaTran.sql
@@ -0,0 +1,123 @@
+-- MySQL dump 10.13  Distrib 5.6.17, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: miniblog
+-- ------------------------------------------------------
+-- Server version	5.6.22
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `comment`
+--
+
+DROP TABLE IF EXISTS `comment`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `comment` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `IdPost` int(11) NOT NULL,
+  `Author` int(11) NOT NULL,
+  `Content` varchar(500) NOT NULL,
+  `Create_date` datetime NOT NULL,
+  `Modify_date` datetime NOT NULL,
+  `Status` enum('Available','Delete') NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_comment_Post_idx` (`IdPost`),
+  KEY `fk_comment_User_idx` (`Author`),
+  CONSTRAINT `fk_comment_Post` FOREIGN KEY (`IdPost`) REFERENCES `post` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_comment_User` FOREIGN KEY (`Author`) REFERENCES `userblog` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `comment`
+--
+
+LOCK TABLES `comment` WRITE;
+/*!40000 ALTER TABLE `comment` DISABLE KEYS */;
+/*!40000 ALTER TABLE `comment` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `post`
+--
+
+DROP TABLE IF EXISTS `post`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `post` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `Author` int(11) NOT NULL,
+  `Title` varchar(100) NOT NULL,
+  `Content` varchar(2000) NOT NULL,
+  `Create_date` datetime NOT NULL,
+  `Modify_date` datetime NOT NULL,
+  `Status` enum('Available','Delete') NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_post_User_idx` (`Author`),
+  FULLTEXT KEY `Title` (`Title`,`Content`),
+  CONSTRAINT `fk_post_User` FOREIGN KEY (`Author`) REFERENCES `userblog` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `post`
+--
+
+LOCK TABLES `post` WRITE;
+/*!40000 ALTER TABLE `post` DISABLE KEYS */;
+/*!40000 ALTER TABLE `post` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `userblog`
+--
+
+DROP TABLE IF EXISTS `userblog`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `userblog` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `Username` varchar(40) DEFAULT NULL,
+  `Password` varchar(40) NOT NULL,
+  `Email` varchar(50) NOT NULL,
+  `Lastname` varchar(40) NOT NULL,
+  `Firstname` varchar(40) NOT NULL,
+  `Gender` varchar(10) NOT NULL,
+  `Birthday` varchar(10) NOT NULL,
+  `Create_date` datetime NOT NULL,
+  `Modify_date` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `Username_UNIQUE` (`Username`),
+  FULLTEXT KEY `Username` (`Username`,`Firstname`,`Lastname`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `userblog`
+--
+
+LOCK TABLES `userblog` WRITE;
+/*!40000 ALTER TABLE `userblog` DISABLE KEYS */;
+/*!40000 ALTER TABLE `userblog` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2015-02-13 14:18:49
